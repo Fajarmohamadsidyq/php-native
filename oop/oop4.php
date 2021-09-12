6661714ed77fe58489279ce4bcd06718d62bf0e1
Fajarmohamadsidyq <fajarmuhamadsidyq@gmail.com>
2021-09-11 22:51:08 -0700

    hallo

--- oop/oop4.php
+++ oop/oop4.php
@@ -9,8 +9,17 @@
         {
             return ($this->sisi * $this->sisi);
         }
-        public function kelil
+        public function kelilingPersegi()
+        {
+            return 4*$this->sisi;
+        }
     }
+    $persegi = new persegi();
+    $persegi->sisi=10;
+    echo "Sisi : " .$persegi->sisi;
+    echo "<br>";
+    echo "Luas Persegi :" .$persegi->luasPersegi(). "<br>";
+    echo "keliling Persegi :" .$persegi->kelilingPersegi(). "<hr>";
 }
 
 ?>
